3528843b20eccf41afe12b73797ffa24bcc46cb7
jeeniferbarbosa <jenniferbarbosa.tech@gmail.com>
2025-03-25 21:56:44 -0300

    Adicionei novos projetos ao repositório

---
+++ Estruta de repetição/Desafio057.py
@@ -0,0 +1,7 @@
+## Exercício Python 57: Faça um programa que leia o sexo de uma pessoa,
+# mas só aceite os valores ‘M’ ou ‘F’. Caso esteja errado, peça a digitação novamente até ter um valor correto
+
+sexo = str(input("Informe seu sexo: [M/F] ")).upper()[0].strip()
+while sexo not in ('MF'):
+    sexo = str(input("Opção inválida. Digite seu sexo novamente: [M/F] ")).upper()[0].strip()
+print(f"Sexo '{sexo}' foi registrado em nosso sitema com sucesso. ")
---
+++ Estruta de repetição/Desafio058.py
@@ -0,0 +1,18 @@
+# Melhore o jogo do DESAFIO 28 onde o computador vai “pensar” em um número entre 0 e 10.
+# Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.
+
+from random import randint
+
+palpites = 0
+pc = randint(0, 10)
+print("Vou pensar em um número de 0 a 10. Tente adivinhar.")
+while True:
+    jogador = int(input("Em que número eu pensei? "))
+    palpites += 1
+    if jogador == pc:
+        print(f"Parabéns, você acertou! Seus palpites até o acerto foram {palpites}")
+    else:
+        if jogador < pc:
+            print("É um número maior... Tente novamente.")
+        if jogador > pc:
+            print("É um número menor... Tente novamente. ")
