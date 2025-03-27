110010f898749b7a1a5232ef69e507c1814ee274
jeeniferbarbosa <jenniferbarbosa.tech@gmail.com>
2025-03-26 22:24:01 -0300

    Adicionei novos projetos ao repositório

---
+++ Estruta de repetição/Desafio060.py
@@ -0,0 +1,26 @@
+## Faça um programa que leia um número qualquer e mostre o seu fatorial. Exemplo:
+#5! = 5 x 4 x 3 x 2 x 1 = 120
+
+## USANDO WHILE
+num = int(input("informe um número e veja seu fatorial: "))
+cont = num
+fator = 1
+
+print(f"Calculando {num}! = ", end='')
+
+while cont > 0:
+    print(f"{cont}", end='')
+    print(f" x " if cont > 1 else " = ", end='')
+    fator *= cont
+    cont -= 1
+print(fator)
+
+## USANDO FOR
+num = int(input("informe um número e veja seu fatorial: "))
+fator = 1
+
+for cont in range(num, 0, -1):
+    print(f"{cont}", end='')
+    print(f" x " if cont > 1 else " = ", end='')
+    fator *= cont
+print(f"{fator}")
