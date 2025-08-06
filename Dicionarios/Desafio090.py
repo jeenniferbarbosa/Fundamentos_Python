9c5620a7b73a53acee7446a592275501851fb7b8
jeeniferbarbosa <jenniferbarbosa.tech@gmail.com>
2025-08-05 21:54:51 -0300

    Adicionei projetos ao meu repositorio

--- Dicionarios/Desafio090.py
+++ Dicionarios/Desafio090.py
@@ -1,6 +1,3 @@
-# Exercício Python 090: Faça um programa que leia nome e média de um aluno,
-# guardando também a situação em um dicionário. No final, mostre o conteúdo da estrutura na tela.
-
 dados = dict()
 
 dados['nome'] = str(input("Nome do aluno: "))
